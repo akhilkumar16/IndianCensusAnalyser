27fe0b79109f414bc16c35a9926bd3f1dec029b1
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-22 10:48:30 +0530

    [Akhil] Census

---
+++ IndianCensusAnalyser/CensusAnalyserBase.cs
@@ -0,0 +1,12 @@
+﻿namespace IndianCensusAnalyser.POCO
+{
+    public class CensusAnalyserBase
+    {
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath,
+            string dataHeaders)
+        {
+            dataMap = new CsvAdaptorFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
